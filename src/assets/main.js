abdccce6f11ecd3c8295a97b9884f69a685a61b7
Jhonatan-Cardona <tatancarduar@gmail.com>
2023-02-23 09:57:54 -0500

    Commit Proyecto5

---
+++ src/assets/main.js
@@ -0,0 +1,44 @@
+const API = 'https://youtube-v31.p.rapidapi.com/playlistItems?playlistId=PLunjHizQT1RxzlVB7lCuh5XN2B0uS-Aae&part=snippet&maxResults=9'
+
+const content = null || document.getElementById('content');
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '6aa8de0972msh544db52a408dac3p1ba30ajsn88b0949aa802',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+
+async function fetchData(urlApi) {
+    const response = await fetch(urlApi, options);
+    const data = await response.json();
+    return data;
+}
+
+// creamos una funcion que se llama asi misma.
+
+(async () => {
+    try {
+      const videos = await fetchData(API);
+      let view = `
+      ${videos.items.map(video => `
+        <div class="group relative">
+          <div
+            class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+            <img src="${video.snippet.thumbnail.high.url}" alt="${video.snippet.description}" class="w-full">
+          </div>
+          <div class="mt-4 flex justify-between">
+            <h3 class="text-sm text-gray-700">
+              <span aria-hidden="true" class="absolute inset-0"></span>
+              ${video.snippet.title}
+            </h3>
+          </div>
+        </div>
+      `).slice(0,4).join('')}
+  
+      `;
+    } catch {
+  
+    }
+  })();
